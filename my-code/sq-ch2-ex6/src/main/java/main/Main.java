69c44ce1d9a7e5582ca2cf3d09fd4538ed332e59
qeewpi <leyashml@gmail.com>
2025-11-05 16:23:51 +0800

    feat: completed `sq-ch2-ex6`

---
+++ my-code/sq-ch2-ex6/src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p);
+        System.out.println(p.getName());
+    }
+}
