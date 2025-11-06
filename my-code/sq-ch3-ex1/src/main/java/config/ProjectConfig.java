d4820651d55f050f44030b25ebf19e0084f95ffa
qeewpi <leyashml@gmail.com>
2025-11-06 17:08:56 +0800

    feat: completed `sq-ch3-ex1`

---
+++ my-code/sq-ch3-ex1/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import beans.Parrot;
+import beans.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+
+        return p;
+    }
+
+    @Bean
+    Person person() {
+        Person p = new Person();
+
+        p.setName("Ella");
+
+        return p;
+    }
+}
