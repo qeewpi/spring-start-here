f553adc528c5e269c191a2f64da539fc01e646fa
qeewpi <leyashml@gmail.com>
2025-11-13 18:57:14 +0800

    feat: completed `sq-ch3-ex9`

---
+++ my-code/sq-ch3-ex9/src/main/java/beans/Person.java
@@ -0,0 +1,29 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name;
+
+    private final Parrot parrot;
+
+    public Person(Parrot parrot2) {
+        this.parrot = parrot2;
+        this.name = "Ella";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+}
