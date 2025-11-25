435714ce207e8abfd39338f141c6f2a13456224d
qeewpi <leyashml@gmail.com>
2025-11-25 22:11:31 +0800

    feat: complete `sq-ch4-ex7`

---
+++ my-code/sq-ch4-ex7/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// The @Configuration annotation marks the configuration class.
+@Configuration
+/* We use the @ComponentScan annotation to tell Spring in which
+packages to search for the classes annotated with stereotype
+annotations. Observe that the model package is not specified because it
+doesn’t contain classes annotated with stereotype annotations. */
+@ComponentScan(basePackages = {"proxies", "repositories", "services"})
+public class ProjectConfig {
+}
---
+++ my-code/sq-ch4-ex7/src/main/java/services/CommentService.java
@@ -0,0 +1,26 @@
+package services;
+
+import model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import proxies.CommentNotificationProxy;
+import repositories.CommentRepository;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    /* For each parameter where we want to use a specific
+    implementation, we annotate the parameter with @Qualifier. */
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
